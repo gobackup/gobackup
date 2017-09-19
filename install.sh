999387845e0aea07f98db72dbe5f83b6e786b146
Jason Lee <huacnlee@gmail.com>
2017-09-19 15:02:25 +0800

    Update install.sh to 0.3.0

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-curl -Lo gobackup-linux-amd64.tar.gz https://github.com/huacnlee/gobackup/releases/download/0.2.0/gobackup-linux-amd64.tar.gz
+curl -Lo gobackup-linux-amd64.tar.gz https://github.com/huacnlee/gobackup/releases/download/0.3.0/gobackup-linux-amd64.tar.gz
 tar zxf gobackup-linux-amd64.tar.gz && sudo mv gobackup /usr/local/bin/gobackup && rm gobackup-linux-amd64.tar.gz
 mkdir -p ~/.gobackup && touch ~/.gobackup/gobackup.yml
