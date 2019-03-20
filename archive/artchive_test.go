ca31d612b3b536d0f4d322d6c15d243bf1c857f5
Griffin Qiu <griffinqiu@gmail.com>
2019-03-21 00:04:56 +0800

    Fixed CI

--- archive/artchive_test.go
+++ archive/artchive_test.go
@@ -34,8 +34,8 @@ func TestOptions(t *testing.T) {
 	opts := options(dumpPath, excludes, includes)
 	cmd := strings.Join(opts, " ")
 	if helper.IsGnuTar {
-		assert.Equal(t, cmd, "--ignore-failed-read -cPf ~/work/dir/archive.tar --exclude='/hello/world' --exclude='/cc/111' /foo/bar/dar /bar/foo /ddd")
+		assert.Equal(t, cmd, "--ignore-failed-read -cPf ~/work/dir/archive.tar --exclude=/hello/world --exclude=/cc/111 /foo/bar/dar /bar/foo /ddd")
 	} else {
-		assert.Equal(t, cmd, "-cPf ~/work/dir/archive.tar --exclude='/hello/world' --exclude='/cc/111' /foo/bar/dar /bar/foo /ddd")
+		assert.Equal(t, cmd, "-cPf ~/work/dir/archive.tar --exclude=/hello/world --exclude=/cc/111 /foo/bar/dar /bar/foo /ddd")
 	}
 }
