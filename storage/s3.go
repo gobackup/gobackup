82a5b4529655ef6b7a199ba5fc2245a4578afe18
Zach Cheung <kuroro.zhang@gmail.com>
2022-12-04 23:17:24 -0800

    s3: support directory

--- storage/s3.go
+++ storage/s3.go
@@ -5,7 +5,7 @@ import (
 	"math"
 	"net/http"
 	"os"
-	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -145,67 +145,81 @@ func (s *S3) close() {
 func (s *S3) upload(fileKey string) (err error) {
 	logger := logger.Tag(s.providerName())
 
-	f, err := os.Open(s.archivePath)
-	if err != nil {
-		return fmt.Errorf("failed to open file %q, %v", s.archivePath, err)
+	var fileKeys []string
+	if len(s.fileKeys) != 0 {
+		// directory
+		// 2022.12.04.07.09.47/2022.12.04.07.09.47.tar.xz-000
+		fileKeys = s.fileKeys
+	} else {
+		// file
+		// 2022.12.04.07.09.25.tar.xz
+		fileKeys = append(fileKeys, fileKey)
 	}
 
-	defer f.Close()
-
-	info, err := f.Stat()
-	if err != nil {
-		return fmt.Errorf("failed to get size of file %q, %v", s.archivePath, err)
-	}
-
-	remotePath := path.Join(s.path, fileKey)
-
-	input := &s3manager.UploadInput{
-		Bucket: aws.String(s.bucket),
-		Key:    aws.String(remotePath),
-		Body:   f,
-	}
-
-	logger.Info(fmt.Sprintf("-> Uploading (%d MiB)...", info.Size()/(1024*1024)))
-
-	start := time.Now()
-
-	result, err := s.client.Upload(input, func(uploader *s3manager.Uploader) {
-		// set the part size as low as possible to avoid timeouts and aborts
-		// also set concurrency to 1 for the same reason
-		var partSize int64 = 5242880 // 5MiB
-		maxParts := math.Ceil(float64(info.Size() / partSize))
-
-		// 10000 parts is the limit for AWS S3. If the resulting number of parts would exceed that limit, increase the
-		// part size as much as needed but as little possible
-		if maxParts > 10000 {
-			partSize = int64(math.Ceil(float64(info.Size()) / 10000))
+	for _, key := range fileKeys {
+		filePath := filepath.Join(filepath.Dir(s.archivePath), key)
+		f, err := os.Open(filePath)
+		if err != nil {
+			return fmt.Errorf("failed to open file %q, %v", filePath, err)
 		}
 
-		uploader.Concurrency = 1
-		uploader.LeavePartsOnError = false
-		uploader.PartSize = partSize
-	})
+		defer f.Close()
 
-	if err != nil {
-		return fmt.Errorf("failed to upload file, %v", err)
+		info, err := f.Stat()
+		if err != nil {
+			return fmt.Errorf("failed to get size of file %q, %v", filePath, err)
+		}
+
+		remotePath := filepath.Join(s.path, key)
+
+		input := &s3manager.UploadInput{
+			Bucket: aws.String(s.bucket),
+			Key:    aws.String(remotePath),
+			Body:   f,
+		}
+
+		logger.Info(fmt.Sprintf("-> Uploading (%d MiB)...", info.Size()/(1024*1024)))
+
+		start := time.Now()
+
+		result, err := s.client.Upload(input, func(uploader *s3manager.Uploader) {
+			// set the part size as low as possible to avoid timeouts and aborts
+			// also set concurrency to 1 for the same reason
+			var partSize int64 = 5242880 // 5MiB
+			maxParts := math.Ceil(float64(info.Size() / partSize))
+
+			// 10000 parts is the limit for AWS S3. If the resulting number of parts would exceed that limit, increase the
+			// part size as much as needed but as little possible
+			if maxParts > 10000 {
+				partSize = int64(math.Ceil(float64(info.Size()) / 10000))
+			}
+
+			uploader.Concurrency = 1
+			uploader.LeavePartsOnError = false
+			uploader.PartSize = partSize
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to upload file, %v", err)
+		}
+
+		t := time.Now()
+		elapsed := t.Sub(start)
+
+		logger.Info("=>", result.Location)
+		if s.Service == "s3" {
+			logger.Info("=>", fmt.Sprintf("s3://%s/%s", s.bucket, remotePath))
+		}
+		rate := math.Ceil(float64(info.Size()) / (elapsed.Seconds() * 1024 * 1024))
+
+		logger.Info(fmt.Sprintf("Duration %v, rate %.1f MiB/s", durafmt.Parse(elapsed).LimitFirstN(2).String(), rate))
 	}
-
-	t := time.Now()
-	elapsed := t.Sub(start)
-
-	logger.Info("=>", result.Location)
-	if s.Service == "s3" {
-		logger.Info("=>", fmt.Sprintf("s3://%s/%s", s.bucket, remotePath))
-	}
-	rate := math.Ceil(float64(info.Size()) / (elapsed.Seconds() * 1024 * 1024))
-
-	logger.Info(fmt.Sprintf("Duration %v, rate %.1f MiB/s", durafmt.Parse(elapsed).LimitFirstN(2).String(), rate))
 
 	return nil
 }
 
 func (s *S3) delete(fileKey string) (err error) {
-	remotePath := path.Join(s.path, fileKey)
+	remotePath := filepath.Join(s.path, fileKey)
 	input := &s3.DeleteObjectInput{
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(remotePath),
