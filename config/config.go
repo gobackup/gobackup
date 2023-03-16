768f70a043e980bd99ef1f884049d22b92adbdb6
Jason Lee <huacnlee@gmail.com>
2023-03-16 21:55:20 +0800

    Remove deprecated `store_with` config on model. #144

--- config/config.go
+++ config/config.go
@@ -238,16 +238,6 @@ func loadDatabasesConfig(model *ModelConfig) {
 
 func loadStoragesConfig(model *ModelConfig) {
 	storageConfigs := map[string]SubConfig{}
-	// Backward compatible with `store_with` config
-	storeWith := model.Viper.Sub("store_with")
-	if storeWith != nil {
-		logger.Warn(`[Deprecated] "store_with" is deprecated now, please use "storages" which supports multiple storages. Cycler config which usually located in "~/.gobackup/cycler" and named "MODEL.json" should be renamed to "MODEL_STORAGENAME.json", or cycler will start from scratch.`)
-		storageConfigs["store_with"] = SubConfig{
-			Name:  "",
-			Type:  model.Viper.GetString("store_with.type"),
-			Viper: model.Viper.Sub("store_with"),
-		}
-	}
 
 	subViper := model.Viper.Sub("storages")
 	for key := range model.Viper.GetStringMap("storages") {
