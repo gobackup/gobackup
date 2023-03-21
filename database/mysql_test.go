99b2af2185b6ee625de7b0532dc6795cb1ccec00
Jason Lee <huacnlee@gmail.com>
2023-03-21 10:35:51 +0800

    Fix test

--- database/mysql_test.go
+++ database/mysql_test.go
@@ -19,7 +19,7 @@ func TestMySQL_init(t *testing.T) {
 	viper.Set("password", "pass1")
 	viper.Set("tables", []string{"foo", "bar"})
 	viper.Set("exclude_tables", []string{"aa", "bb"})
-	viper.Set("args", "--foo --bar --dar")
+	viper.Set("args", "--a1 --a2 --a3")
 
 	base := newBase(
 		config.ModelConfig{
@@ -40,12 +40,7 @@ func TestMySQL_init(t *testing.T) {
 	err := db.init()
 	assert.NoError(t, err)
 	script := db.build()
-	assert.Equal(t, script, "mysqldump --host 1.2.3.4 --port 1234 -u user1 -ppass1 --ignore-table=my_db.aa --ignore-table=my_db.bb --foo --bar --dar my_db foo bar --result-file=/data/backups/mysql/mysql1/my_db.sql")
-
-	viper.Set("additional_options", "--bar --foo")
-	err = db.init()
-	assert.NoError(t, err)
-	assert.Equal(t, db.build(), "mysqldump --host 1.2.3.4 --port 1234 -u user1 -ppass1 --ignore-table=my_db.aa --ignore-table=my_db.bb --bar --foo my_db foo bar --result-file=/data/backups/mysql/mysql1/my_db.sql")
+	assert.Equal(t, script, "mysqldump --host 1.2.3.4 --port 1234 -u user1 -ppass1 --ignore-table=my_db.aa --ignore-table=my_db.bb --a1 --a2 --a3 my_db foo bar --result-file=/data/backups/mysql/mysql1/my_db.sql")
 }
 
 func TestMySQL_dumpArgsWithAdditionalOptions(t *testing.T) {
