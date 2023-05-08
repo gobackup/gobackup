d44d9ca09bea58fffedb8a04ea9f2d30643dabf4
Jason Lee <huacnlee@gmail.com>
2023-05-08 15:09:56 +0800

    Fix SQLite dump command again, you `.output` command.
    And raise error, if SQLite not have `path` config.
    
    Ref #150

--- database/sqlite.go
+++ database/sqlite.go
@@ -25,6 +25,11 @@ func (db *SQLite) init() error {
 	viper := db.viper
 
 	db.path = helper.ExplandHome(viper.GetString("path"))
+
+	if len(db.path) == 0 {
+		return fmt.Errorf("SQLite `path` is required, you must special the path of the `.sqlite3` file")
+	}
+
 	db.database = strings.TrimSuffix(filepath.Base(db.path), filepath.Ext(db.path))
 
 	db._dumpFilePath = filepath.Join(db.dumpPath, db.database+".sql")
@@ -35,7 +40,8 @@ func (db *SQLite) init() error {
 func (db *SQLite) buildArgs() []string {
 	args := []string{
 		db.path,
-		fmt.Sprintf(".dump > %s", db._dumpFilePath),
+		fmt.Sprintf(".output %s", db._dumpFilePath),
+		".dump",
 	}
 	return args
 }
