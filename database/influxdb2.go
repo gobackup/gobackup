71c860d52bb6284d9cca89e26ad1d76ade24d62b
Pavel Zavora <pavel.zavora@bonitoo.io>
2023-12-05 13:37:42 +0100

    feat: support influxdb v2 through influx cli

---
+++ database/influxdb2.go
@@ -0,0 +1,96 @@
+package database
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gobackup/gobackup/helper"
+	"github.com/gobackup/gobackup/logger"
+)
+
+// MongoDB database
+//
+// type: mongodb
+// host: 127.0.0.1
+// port: 27017
+// database:
+// username:
+// password:
+// authdb:
+// exclude_tables:
+// oplog: false
+// args:
+type InfluxDB2 struct {
+	Base
+	host       string
+	token      string
+	bucket     string
+	bucketId   string
+	org        string
+	orgId      string
+	skipVerify bool
+	httpDebug  bool
+}
+
+func (db *InfluxDB2) init() (err error) {
+	viper := db.viper
+	viper.SetDefault("skipVerify", false)
+	viper.SetDefault("httpDebug", false)
+
+	db.host = viper.GetString("host")
+	db.token = viper.GetString("token")
+	db.bucket = viper.GetString("bucket")
+	db.bucketId = viper.GetString("bucketId")
+	db.org = viper.GetString("org")
+	db.orgId = viper.GetString("orgId")
+	db.skipVerify = viper.GetBool("skipVerify")
+
+	if db.host == "" {
+		return fmt.Errorf("no host specified in influxdb2 configuration: %s", db.name)
+	}
+	if db.token == "" {
+		return fmt.Errorf("no token specified in influxdb2 configuration: %s", db.name)
+	}
+
+	return nil
+}
+
+func (db *InfluxDB2) build() string {
+	opts := make([]string, 0, 15)
+	opts = append(opts, "influx backup")
+	opts = append(opts, "--host="+db.host+"")
+	opts = append(opts, "--token="+db.token+"")
+	if db.bucket != "" {
+		opts = append(opts, "--bucket="+db.bucket+"")
+	}
+	if db.bucketId != "" {
+		opts = append(opts, "--bucket-id="+db.bucketId+"")
+	}
+	if db.org != "" {
+		opts = append(opts, "--org="+db.org+"")
+	}
+	if db.orgId != "" {
+		opts = append(opts, "--org-id="+db.orgId+"")
+	}
+	if db.skipVerify {
+		opts = append(opts, "--skip-verify")
+	}
+	if db.httpDebug {
+		opts = append(opts, "--http-debug")
+	}
+	opts = append(opts, db.dumpPath)
+	return strings.Join(opts, " ")
+}
+
+func (db *InfluxDB2) perform() error {
+	logger := logger.Tag("InfluxDB2")
+
+	command := db.build()
+	out, err := helper.Exec(command)
+	if err != nil {
+		return fmt.Errorf("-> Dump error: %s", err)
+	}
+	logger.Info(out)
+	logger.Info("dump path:", db.dumpPath)
+	return nil
+}
