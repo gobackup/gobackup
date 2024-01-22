2f5f6db5358bdfccd2464a4ba73d3699fbc879b0
Jason Lee <huacnlee@gmail.com>
2024-01-22 12:02:04 +0800

    Add `before_script`, `after_script` to model for write hooks around the model performing.
    Fix #222, #213

--- helper/exec.go
+++ helper/exec.go
@@ -18,6 +18,10 @@ var (
 
 // Exec cli commands
 func Exec(command string, args ...string) (output string, err error) {
+	return ExecWithStdio(command, false, args...)
+}
+
+func ExecWithStdio(command string, stdout bool, args ...string) (output string, err error) {
 	commands := spaceRegexp.Split(command, -1)
 	command = commands[0]
 	commandArgs := []string{}
@@ -37,17 +41,21 @@ func Exec(command string, args ...string) (output string, err error) {
 	cmd.Env = os.Environ()
 
 	var stdErr bytes.Buffer
+	var stdOut bytes.Buffer
 	cmd.Stderr = &stdErr
 
-	// logger.Debug(fullCommand, " ", strings.Join(commandArgs, " "))
+	if stdout {
+		cmd.Stdout = os.Stdout
+	} else {
+		cmd.Stdout = &stdOut
+	}
 
-	out, err := cmd.Output()
+	err = cmd.Run()
 	if err != nil {
 		logger.Debug(fullCommand, " ", strings.Join(commandArgs, " "))
 		err = errors.New(stdErr.String())
-		return
 	}
+	output = strings.Trim(stdOut.String(), "\n")
 
-	output = strings.Trim(string(out), "\n")
 	return
 }
--- helper/exec_test.go
+++ helper/exec_test.go
@@ -7,24 +7,34 @@ import (
 )
 
 func TestExec(t *testing.T) {
-	res, err := Exec("head", "-n1", "./exec_test.go")
+	out, err := Exec("head", "-n1", "./exec_test.go")
 	assert.Nil(t, err)
-	assert.Equal(t, res, "package helper")
+	assert.Equal(t, out, "package helper")
 
-	res, err = Exec("head -n1 ./exec_test.go")
+	out, err = Exec("head -n1 ./exec_test.go")
 	assert.Nil(t, err)
-	assert.Equal(t, res, "package helper")
+	assert.Equal(t, out, "package helper")
 
-	res, err = Exec("head  -n1  ./exec_test.go")
+	out, err = Exec("head  -n1  ./exec_test.go")
 	assert.Nil(t, err)
-	assert.Equal(t, res, "package helper")
+	assert.Equal(t, out, "package helper")
 
-	res, err = Exec("head -n1", "./exec_test.go")
+	out, err = Exec("head -n1", "./exec_test.go")
 	assert.Nil(t, err)
-	assert.Equal(t, res, "package helper")
+	assert.Equal(t, out, "package helper")
 
-	res, err = Exec("not-found-command", "foo")
+	out, err = Exec("not-found-command", "foo")
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "not-found-command cannot be found")
-	assert.Empty(t, res)
+	assert.Empty(t, out)
+}
+
+func TestExecWithStdio(t *testing.T) {
+	out, err := ExecWithStdio("head -n1", false, "./exec_test.go")
+	assert.Nil(t, err)
+	assert.Equal(t, out, "package helper")
+
+	out, err = ExecWithStdio("head -n1", true, "./exec_test.go")
+	assert.Nil(t, err)
+	assert.Empty(t, out)
 }
