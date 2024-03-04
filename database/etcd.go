48343e7c0b97e35d8e642c34dad42e07d94c7f12
Iman Kermani <imankermani@hotmail.com>
2024-03-05 00:19:14 +0330

    Added backward compatability with `endpoints` settings.

--- database/etcd.go
+++ database/etcd.go
@@ -18,10 +18,13 @@ import (
 //
 //   - type: etcd
 //   - endpoint: localhost:2379
+//     # depricated
+//   - endpoints: [localhost:2379, localhost:22379, localhost:32379]
 //   - args:
 type Etcd struct {
 	Base
 	endpoint      string
+	endpoints     []string
 	args          string
 	_dumpFilePath string
 }
@@ -30,10 +33,21 @@ func (db *Etcd) init() (err error) {
 	viper := db.viper
 
 	db.endpoint = viper.GetString("endpoint")
+	db.endpoints = viper.GetStringSlice("endpoints")
 	db.args = viper.GetString("args")
 
-	if len(db.endpoint) == 0 {
+	if len(db.endpoint) == 0 && len(db.endpoints) == 0 {
 		return fmt.Errorf("etcd endpoint config is required")
+	}
+
+	if len(db.endpoint) > 0 && len(db.endpoints) > 0 {
+		return fmt.Errorf("etcd `endpoint` and `endpoints` config are mutually exclusive")
+	}
+
+	if len(db.endpoint) == 0 && len(db.endpoints) > 0 {
+		logger.Warn("DEPRECATED: `endpoints` is deprecated, use `endpoint` instead.")
+		logger.Warn("The first element of endpoints will be used.")
+		db.endpoint = db.endpoints[0]
 	}
 
 	db._dumpFilePath = path.Join(db.dumpPath + "-" + db.endpoint)
