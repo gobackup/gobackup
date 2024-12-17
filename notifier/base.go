b4449f3408c9882f635e9ffed0c45c1196dc9260
careylipu <lipuemail@163.com>
2024-12-17 13:36:36 +0800

    notifier: Fix GoogleChat read config not enable. (#277)
    Sorry, I forgot to submit the changes to notifier/base.go to add Google
    Chat.
    
    Here's an example Google Chat notifier configuration.
    
        notifiers:
          googlechat:
            type: googlechat
    url: https://chat.googleapis.com/v1/spaces/XXXXX/messages?key=XXXXXX
            method: POST

--- notifier/base.go
+++ notifier/base.go
@@ -64,7 +64,9 @@ func newNotifier(name string, config config.SubConfig) (Notifier, *Base, error) 
 		return NewResend(base), base, nil
 	case "wxwork":
 		return NewWxWork(base), base, nil
-	}
+	case "googlechat":
+	        return NewGoogleChat(base), base, nil
+ 	}
 
 	return nil, nil, fmt.Errorf("Notifier: %s is not supported", name)
 }
