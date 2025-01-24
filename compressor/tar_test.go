fa8bfe0ab203781fc508b76458d64a57f5d73777
Jason Lee <huacnlee@gmail.com>
2025-01-25 00:06:40 +0800

    .

--- compressor/tar_test.go
+++ compressor/tar_test.go
@@ -3,12 +3,25 @@ package compressor
 import (
 	"testing"
 
+	"github.com/gobackup/gobackup/config"
 	"github.com/gobackup/gobackup/helper"
 	"github.com/longbridgeapp/assert"
+	"github.com/spf13/viper"
 )
 
 func TestTar_options(t *testing.T) {
-	tar := &Tar{}
+	viper := viper.New()
+	viper.Set("args", "--foo --bar --dar")
+	base := newBase(config.ModelConfig{
+		CompressWith: config.SubConfig{
+			Type:  "compress_with",
+			Name:  "tar",
+			Viper: viper,
+		},
+	},
+	)
+
+	tar := &Tar{base}
 	opts := tar.options()
 	if helper.IsGnuTar {
 		assert.Equal(t, opts[0], "--ignore-failed-read")
@@ -17,6 +30,7 @@ func TestTar_options(t *testing.T) {
 	} else {
 		assert.Equal(t, opts[0], "-a")
 		assert.Equal(t, opts[1], "-cf")
+		assert.Equal(t, opts[2], "--foo --bar --dar")
 	}
 
 }
