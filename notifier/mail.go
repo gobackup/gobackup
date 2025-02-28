815c9caa48f2621715968a57905a0f07939e9f8c
ionling <soarvision@outlook.com>
2025-02-28 09:58:19 +0800

    Support TLS for mail notifier

--- notifier/mail.go
+++ notifier/mail.go
@@ -1,7 +1,9 @@
 package notifier
 
 import (
+	"crypto/tls"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"net/smtp"
 	"sort"
@@ -16,6 +18,7 @@ type Mail struct {
 	password string
 	host     string
 	port     string
+	useTLS   bool
 }
 
 func NewMail(base *Base) (*Mail, error) {
@@ -38,6 +41,7 @@ func NewMail(base *Base) (*Mail, error) {
 		from:     from,
 		host:     base.viper.GetString("host"),
 		port:     base.viper.GetString("port"),
+		useTLS:   base.viper.GetBool("useTLS"),
 	}, nil
 }
 
@@ -83,11 +87,55 @@ func (s *Mail) notify(title string, message string) error {
 	// Connect to the server, authenticate, set the sender and recipient,
 	// and send the email all in one step.
 	to := s.to
+	msg := s.buildBody(title, message)
 
-	err := smtp.SendMail(s.getAddr(), auth, s.from, to, []byte(s.buildBody(title, message)))
+	if s.useTLS {
+		return s.SendMailTLS(auth, []byte(msg))
+	} else {
+		return smtp.SendMail(s.getAddr(), auth, s.from, to, []byte(msg))
+	}
+}
+
+func (s Mail) SendMailTLS(a smtp.Auth, msg []byte) error {
+	conn, err := tls.Dial("tcp", s.getAddr(), nil)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	c, err := smtp.NewClient(conn, s.host)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	if a != nil {
+		ok, _ := c.Extension("AUTH")
+		if !ok {
+			return errors.New("smtp: server doesn't support AUTH")
+		}
+		if err = c.Auth(a); err != nil {
+			return err
+		}
+	}
+	if err = c.Mail(s.from); err != nil {
+		return err
+	}
+	for _, addr := range s.to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
 }
