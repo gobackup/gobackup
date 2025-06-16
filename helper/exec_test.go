5d974393eff6aa5004df1a953ba83dcee5728eb2
Jason Lee <huacnlee@gmail.com>
2025-06-16 18:22:49 +0800

    .

--- helper/exec_test.go
+++ helper/exec_test.go
@@ -40,9 +40,9 @@ func TestExecWithStdio(t *testing.T) {
 }
 
 func TestExecScriptWithStdio(t *testing.T) {
-	out, err := ExecScriptWithStdio("head -n1 ./exec_test.go\n# This is a comment\nhead -n1 ./exec_test.go | wc -c\necho hello world\necho foobar", false)
+	out, err := ExecScriptWithStdio("head -n1 ./exec_test.go\n# This is a comment\nhead -n1 ./exec_test.go | wc -c | sed 's/^[[:space:]]*//'\necho hello world\necho foobar", false)
 	assert.Nil(t, err)
-	assert.Equal(t, out, "package helper\n      15\nhello world\nfoobar")
+	assert.Equal(t, out, "package helper\n15\nhello world\nfoobar")
 
 	out, err = ExecScriptWithStdio("head -n1", true)
 	assert.Nil(t, err)
